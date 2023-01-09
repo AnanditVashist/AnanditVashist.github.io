c1fd04767094fc4037ab0b588cc686aadf79782a
Anandit <anandit46@gmail.com>
2023-01-08 19:13:00 -0500

    add image optimazatio loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	webpack: (cfg) => {
+		cfg.module.rules.push({
+			test: /\.md$/,
+			loader: "frontmatter-markdown-loader",
+			options: { mode: ["react-component"] },
+		});
+		return cfg;
+	},
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([
+	[
+		optimizedImages,
+		{
+			/* config for next-optimized-images */
+		},
+	],
+
+	nextConfig,
+]);
